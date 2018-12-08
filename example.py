ed93027ccb8739f70ba967f31930dd82eeece48a
Shoeb Ahmed <sashoeb94@gmail.com>
2018-12-08 22:29:51 +0530

    Update example code

--- example.py
+++ example.py
@@ -1,19 +1,19 @@
 from app_logger import Logger
 
-LOGGER = Logger(name="test", log_level="debug")
+LOGGER = Logger(name="example_app", log_level="debug")
 try:
-    LOGGER.info("My first log stmt")
-    LOGGER.debug(123456)
-    raise Exception("Some error in code")
+    LOGGER.info("This is an info statement.")
+    LOGGER.debug("This is a debug statement with a value: %s" % str(123.456))
+    raise Exception("Example error")
 except Exception as e:
-    LOGGER.error("Hey this is an error message")
+    LOGGER.error("This is an error message.")
     LOGGER.exception(e)
 
-with Logger(name="test_with", log_level="info") as LOGGER:
+with Logger(name="second_example_app", log_level="error") as LOGGER:
     try:
-        LOGGER.info("My first log stmt")
+        LOGGER.info("This info statement won't be printed")
         LOGGER.debug(123456)
-        raise Exception("Some error in code")
+        a = 0/0                                     # Will print the divide by zero error message
     except Exception as e:
-        LOGGER.error("Hey this is an error message")
+        LOGGER.error("This is the error message for divide by zero error")
         LOGGER.exception(e)
